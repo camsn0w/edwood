8af4f60434226f7119ebefa2696297f67a6d6efc
camsn0w <camhconnell@gmail.com>
2021-06-27 14:29:07 -0600

    Added comments

--- observableeditablebuffer.go
+++ observableeditablebuffer.go
@@ -7,6 +7,7 @@ type observableeditablebuffer struct {
 	observers    map[BufferObserver]struct{} // [private I think]
 }
 
+// AddObserver adds e as an observer for edits to this File.
 func (e *observableeditablebuffer) AddObserver(observer BufferObserver) {
 	if e.observers == nil {
 		e.observers = make(map[BufferObserver]struct{})
@@ -16,6 +17,7 @@ func (e *observableeditablebuffer) AddObserver(observer BufferObserver) {
 
 }
 
+// DelObserver removes e as an observer for edits to this File.
 func (e *observableeditablebuffer) DelObserver(observer BufferObserver) error {
 	if _, exists := e.observers[observer]; exists {
 		delete(e.observers, observer)
@@ -30,34 +32,41 @@ func (e *observableeditablebuffer) DelObserver(observer BufferObserver) error {
 	return fmt.Errorf("can't find editor in File.DelObserver")
 }
 
+// SetCurObserver sets the current observer
 func (e *observableeditablebuffer) SetCurObserver(observer BufferObserver) {
 	e.currobserver = observer
 }
 
+// GetCurObserver gets the current observer and returns a interface{}
 func (e *observableeditablebuffer) GetCurObserver() interface{} {
 	return e.currobserver
 }
 
+// AllObservers preforms tf(all observers...)
 func (e *observableeditablebuffer) AllObservers(tf func(i interface{})) {
 	for t := range e.observers {
 		tf(t)
 	}
 }
 
+// GetObserverSize will return the size of the observer map
 func (e *observableeditablebuffer) GetObserverSize() int {
 	return len(e.observers)
 }
 
+// HasMultipleObservers returns true if their are multiple observers to the File
 func (e *observableeditablebuffer) HasMultipleObservers() bool {
 	return len(e.observers) > 1
 }
 
+// insertOnAll inserts at q0 for all observers in the observer map
 func (e *observableeditablebuffer) insertOnAll(q0 int, r []rune) {
 	e.AllObservers(func(i interface{}) {
 		i.(BufferObserver).inserted(q0, r)
 	})
 }
 
+// delete on all deletes q0 to q1 on all of the observer in the observer map
 func (e *observableeditablebuffer) deleteOnAll(q0 int, q1 int) {
 	e.AllObservers(func(i interface{}) {
 		i.(BufferObserver).deleted(q0, q1)
